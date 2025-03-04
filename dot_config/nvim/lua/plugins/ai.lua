90f1531cd3f433c951856c31141fb7ee2214e55f
Spencer Williams <spencerwi@gmail.com>
2025-03-04 17:16:01 -0500

    Update codecompanion.nvim and add MCPHub.nvim

--- dot_config/nvim/lua/plugins/ai.lua
+++ dot_config/nvim/lua/plugins/ai.lua
@@ -8,7 +8,17 @@ return {
 				},
 				strategies = {
 					chat = {
-						adapter = 'openrouter_gemini'
+						adapter = 'openrouter_gemini',
+						tools = {
+							["mcp"] = {
+								callback = require("mcphub.extensions.codecompanion"),
+								description = "Call tools and resources from the MCP Servers",
+								opts = {
+									-- user_approval = true,
+									requires_approval = true
+								}
+							}
+						}
 					},
 					inline = {
 						adapter = 'openrouter_gemini'
@@ -20,7 +30,8 @@ return {
 							name = 'openrouter_gemini',
 							url = 'https://openrouter.ai/api/v1/chat/completions',
 							env = {
-								api_key = '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'
+								url = 'https://openrouter.ai/api/v1/chat/completions',
+								api_key = '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'
 							},
 							schema = {
 								model = {
@@ -47,7 +58,37 @@ return {
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-treesitter/nvim-treesitter",
-			"FeiyouG/commander.nvim"
+			"FeiyouG/commander.nvim",
+			"ravitemer/mcphub.nvim"
 		},
+	},
+	{
+		"ravitemer/mcphub.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",  -- Required for Job and HTTP requests
+		},
+		build = "npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module
+		config = function()
+			require("mcphub").setup({
+				-- Required options
+				port = 3000,  -- Port for MCP Hub server
+				config = vim.fn.expand("~/.config/mcpservers.json"),  -- Absolute path to config file
+
+				-- Optional options
+				on_ready = function(hub)
+					-- Called when hub is ready
+				end,
+				on_error = function(err)
+					-- Called on errors
+				end,
+				shutdown_delay = 0, -- Wait 0ms before shutting down server after last client exits
+				log = {
+					level = vim.log.levels.WARN,
+					to_file = false,
+					file_path = nil,
+					prefix = "MCPHub"
+				},
+			})
+		end
 	}
 }
